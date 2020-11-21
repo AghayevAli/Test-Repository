2a9f91bb42178ba45b426e4a62165704079443e7
Ali C. Agayev <Ali.Agayev@kapitalbank.az>
2020-11-21 23:43:19 +0400

    Commit from first branch for conflict with second branch

--- src/model/DoctorModel.java
+++ src/model/DoctorModel.java
@@ -2,6 +2,10 @@ package model;
 
 public class DoctorModel {
 
+    public static void main(String[] args) {
+        System.out.println("Birinci branchdan hamiya salam bilirem conflict olacagh ona gore yaziram ele");
+    }
+
     private String name;
 
     private String surname;
