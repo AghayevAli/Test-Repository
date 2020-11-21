107504b71fedb4a8f2779dacc60e374e340742f3
Ali C. Agayev <Ali.Agayev@kapitalbank.az>
2020-11-21 23:44:54 +0400

    Gedirem conflicte

--- src/model/DoctorModel.java
+++ src/model/DoctorModel.java
@@ -2,7 +2,11 @@ package model;
 
 public class DoctorModel {
 
+    public static void main(String[] args) {
+        System.out.println("Gonder gelsin");
+    }
+
     private String name;
 
-    private Stirng surname;
+    private String  surname;
 }
